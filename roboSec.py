5ba5fbe00f029bfd09688a6b18a49e03e2ff25cc
Lucascvlh <luikinha_henrique@hotmail.com>
2022-10-13 22:58:51 -0300

    Primeiro Robo

---
+++ roboSec.py
@@ -0,0 +1,137 @@
+import pyautogui
+import time
+
+# X padrão do formulário
+xKirk = 450
+
+# Y padrão Planilha
+# 270 inicia
+yPlanilha = 310
+contadorChamado = 0
+while yPlanilha <= 410:
+    # Resolução 1920 x 1080
+    inicio = time.time()
+    pyautogui.click(114, 171)
+    pyautogui.sleep(2)
+    # Solicitação de pagamentos
+    pyautogui.click(759, 295)
+    pyautogui.sleep(2)
+    # Sim
+    pyautogui.click(850, 599)
+    pyautogui.sleep(2)
+    # Clips
+    pyautogui.click(669, 976)
+    pyautogui.sleep(0.25)
+    # Empresa
+    pyautogui.click(xKirk, 485)
+    pyautogui.sleep(0.25)
+    # Magazine Luiza
+    pyautogui.click(xKirk, 675)
+    pyautogui.sleep(0.25)
+    # Tipo NF
+    pyautogui.click(xKirk, 574)
+    pyautogui.sleep(0.25)
+    # Prestação de serviço
+    pyautogui.click(xKirk, 766)
+    pyautogui.sleep(0.25)
+    # Pegando a PO
+    pyautogui.click(-673, yPlanilha)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.sleep(0.25)
+    # Colocando no kirk
+    pyautogui.click(xKirk, 631)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(0.25)
+    # Prestação de serviço
+    pyautogui.click(xKirk, 703)
+    pyautogui.sleep(0.25)
+    # PJ ou CNPJ
+    pyautogui.click(xKirk, 783)
+    pyautogui.sleep(0.25)
+    # Número recibo
+    pyautogui.click(xKirk, 772)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(0.25)
+    # Método Pagamento
+    pyautogui.click(xKirk, 842)
+    pyautogui.sleep(0.25)
+    # Depósito
+    pyautogui.click(xKirk, 958)
+    pyautogui.sleep(0.25)
+    # Pegando Data de vencimento
+    pyautogui.click(-818, yPlanilha)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.sleep(0.25)
+    # Inserindo a data de vencimento no kirk
+    pyautogui.click(xKirk, 971)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(0.25)
+    pyautogui.press('enter')
+    # Scroll down
+    pyautogui.scroll(-1000)
+    pyautogui.sleep(0.25)
+    # Desconto?
+    pyautogui.click(xKirk, 567)
+    pyautogui.sleep(0.25)
+    # Não
+    pyautogui.click(xKirk, 679)
+    pyautogui.sleep(0.25)
+    # Pegando o numero da NF
+    pyautogui.click(-1022, yPlanilha)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.sleep(0.25)
+    # Procurar por NF no google drive
+    pyautogui.click(1472, 183)
+    pyautogui.click(1650, 118, clicks=2, interval=0.25)
+    pyautogui.press('backspace')
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(5)
+    # Pegando a NF
+    pyautogui.click(1650, 157)
+    pyautogui.sleep(5)
+    # 3 pontinho do download
+    pyautogui.click(2525, 106)
+    pyautogui.sleep(1)
+    # Fazer download
+    pyautogui.click(2311, 190)
+    pyautogui.sleep(5)
+    # Adicionar arquivo
+    pyautogui.click(824, 894)
+    pyautogui.sleep(3)
+    # Selecionando o arquivo
+    pyautogui.click(235, 186, clicks=2, interval=0.25)
+    pyautogui.sleep(1)
+    # Fechar aba no computador do lucas
+    pyautogui.click(1753, 19)
+    pyautogui.sleep(0.25)
+    # Scroll down
+    pyautogui.click(815, 945)
+    pyautogui.scroll(-200)
+    pyautogui.sleep(0.25)
+    # Enviar
+    pyautogui.click(854, 974)
+    pyautogui.sleep(5)
+    # Pegar o número do chamado
+    pyautogui.click(698, 293, clicks=2, interval=0.25)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.sleep(0.25)
+    # Colocando na planilha
+    pyautogui.click(-609, yPlanilha)
+    pyautogui.hotkey('ctrl', 'v')
+    pyautogui.sleep(0.25)
+    # Pegando data de vencimento
+    pyautogui.click(-818, yPlanilha)
+    pyautogui.hotkey('ctrl', 'c')
+    pyautogui.sleep(0.25)
+    # Inserindo a data de vencimento do chamado
+    pyautogui.click(-536, yPlanilha)
+    pyautogui.hotkey('ctrl', 'shift', 'v')
+    pyautogui.sleep(0.25)
+
+    contadorChamado = contadorChamado + 1
+    yPlanilha = yPlanilha + 20
+    fim = time.time()
+
+
+print(round(fim - inicio, 2)+" Segundos e teve " +
+      str(contadorChamado) + " chamados abertos")
